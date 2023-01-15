2b6eaaf671856e5073645624dae53bad5fe00fb1
Alberth Rumbo <ajrumbo29@hotmail.com>
2023-01-15 08:53:09 -0500

    API DE PRUEBAS PAYPAL

--- controller/controller.js
+++ controller/controller.js
@@ -1,27 +1,64 @@
 import axios from "axios";
 
-const auth = { user: process.env.PAYPAL_CLIENT, pass: process.env.PAYPAL_SECRET }
 
 
-const creaatePayment = (req, res) => {
-    const body = {
-        intent: 'CAPTURE',
-        purchase_units: [{
-            amount: {
-                currency_code: 'USD', //https://developer.paypal.com/docs/api/reference/currency-codes/
-                value: '115'
+const creaatePayment = async (req, res) => {
+    
+    const respuesta = await axios({
+        method: 'post',
+        url: `${process.env.PAYPAL_API}/v2/checkout/orders`,
+        data: {
+            'intent': 'CAPTURE',
+            'purchase_units': [{
+                'amount': {
+                    'currency_code': 'USD', //https://developer.paypal.com/docs/api/reference/currency-codes/
+                    'value': '115'
+                }
+            }],
+            'application_context': {
+                'brand_name': `MiTienda.com`,
+                'landing_page': 'NO_PREFERENCE', // Default, para mas informacion https://developer.paypal.com/docs/api/orders/v2/#definition-order_application_context
+                'user_action': 'PAY_NOW', // Accion para que en paypal muestre el monto del pago
+                'return_url': `http://localhost:4000/execute-payment`, // Url despues de realizar el pago
+                'cancel_url': `http://localhost:4000/cancel-payment` // Url despues de realizar el pago
             }
-        }],
-        application_context: {
-            brand_name: `MiTienda.com`,
-            landing_page: 'NO_PREFERENCE', // Default, para mas informacion https://developer.paypal.com/docs/api/orders/v2/#definition-order_application_context
-            user_action: 'PAY_NOW', // Accion para que en paypal muestre el monto del pago
-            return_url: `http://localhost:3000/execute-payment`, // Url despues de realizar el pago
-            cancel_url: `http://localhost:3000/cancel-payment` // Url despues de realizar el pago
-        }
+        },
+        auth: { username: process.env.PAYPAL_CLIENT, password: process.env.PAYPAL_SECRET }
+
+    });
+
+    res.json(respuesta.data);
+
+
+}
+
+const executePayment = async (req, res) => {
+    const {token,PayerID} = req.query;
+    
+    try {
+        const respuesta = await axios({
+            method: 'post',
+            url: `${process.env.PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+            data: {},
+            auth: { username: process.env.PAYPAL_CLIENT, password: process.env.PAYPAL_SECRET }
+        });
+
+        res.json({data: respuesta.data});
+    } catch (error) {
+        console.log(error)
     }
+    
+
+    
+}
+
+const cancelPayment = (req, res) => {
+
+    res.send({msg: 'Pago cancelado'});
 }
 
 export {
-    creaatePayment
+    creaatePayment,
+    cancelPayment,
+    executePayment
 }
--- router/router.js
+++ router/router.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { creaatePayment } from "../controller/controller.js";
+import { creaatePayment, executePayment, cancelPayment } from "../controller/controller.js";
 
 const router = Router();
 
-router.get('/', (req, res) => { res.json({msg: 'Hola desde Router'}) });
+
+router.get('/create-payment', creaatePayment);
+router.get('/execute-payment', executePayment);
+router.get('/cancel-payment', cancelPayment);
 
 export default router;
